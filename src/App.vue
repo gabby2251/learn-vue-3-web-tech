6382f12fe7a7936edb12604adacbe10b470e75ae
gabby2251 <gabby11jr@gmail.com>
2025-03-11 20:06:00 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,13 @@
 <template>
-  <div>
-    <StudentList :list="list">
-      <template #default="{stu}">
-        <span :class="{cursed: stu.name == 'John'}">
-          {{ stu.name  }}
-        </span>
-      </template>
-    </StudentList>
-
-
-
-    <hr>
-
-    <el-table :data="todoList" stripe border style="width: 100%">
-     <el-table-column prop="userId" label="User ID" width="180" />
-      <el-table-column prop="id" label="ID" width="180" />
-      <el-table-column prop="title" label="Title" />
-      <el-table-column prop="completed" label="Status">
-        <template v-slot:default="slotProps">
-          <el-tag type="success" v-if="slotProps.row.completed">Completed</el-tag>
-          <el-tag type="danger" v-else>Incomplete</el-tag>
-        </template>
-      </el-table-column>
-  </el-table>
-  </div>
+  <AlertBox>Invalid username or password. Please try again.</AlertBox>
+  <AlertBox></AlertBox>
+  <AlertBox>
+    <h1>The request timed out. Please refresh the page and try again.</h1>
+  </AlertBox>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
-import StudentList from './StudentList.vue';
-
-
-let todoList  = ref();
-
-async function getTodoList(){
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
-  const data = await response.json();
-  console.log(data)
-  todoList.value = data;
-}
-
-onMounted(()=>{
-  getTodoList();
-})
-
+import AlertBox from './AlertBox.vue'
 </script>
 
-<style  scoped>
-  .cursed{
-    color:red;
-  }
-</style>
+<style lang="scss" scoped></style>
