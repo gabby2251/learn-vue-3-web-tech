4eb60b194a52ab1ce2a47e4320531f6704ffb9d2
gabby2251 <gabby11jr@gmail.com>
2025-03-11 21:17:35 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,7 @@
 <template>
-  <AlertBox>Invalid username or password. Please try again.</AlertBox>
-  <AlertBox></AlertBox>
-  <AlertBox>
-    <h1>The request timed out. Please refresh the page and try again.</h1>
-  </AlertBox>
+  <div>Hello, Vue!</div>
 </template>
 
-<script setup>
-import AlertBox from './AlertBox.vue'
-</script>
+<script setup></script>
 
 <style lang="scss" scoped></style>
